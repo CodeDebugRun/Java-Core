074193fc864468c025d2c7d44cf2eea3873176db
CodeDebugRun <c7105erhan@gmail.com>
2022-07-01 20:37:40 +0300

    Some Changes

--- src/Assignment02_Methods/A01_TakeNumber.java
+++ src/Assignment02_Methods/A01_TakeNumber.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class A01_TakeNumber {
     /*Kullanicidan bir sayi alin . Bu sayinin tek mi cift mi oldugunu
      * sifirdan buyuk mu kucuk mu oldugunu ayrica 100 den buyukse birler
-     * onlar ve yuzler basamagindaki rakamlarin toplamini 100 den kucukse sadece 1 ler basamagini
+     * 3 basamakli onlar ve yuzler basamagindaki rakamlarin toplamini 100 den kucukse sadece 1 ler basamagini
      * yazdiran 3 method olusturun*/
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -29,7 +29,7 @@ public class A01_TakeNumber {
     }
     public static String addDigits (int num1){
         num1 = Math.abs(num1);
-        int sum = num1<100 ? num1/10 : num1 < 1000 ? num1/100 + (num1/10)%10 + num1%10 : num1;
+        int sum = num1<100 ? num1%10 : num1 < 1000 ? num1/100 + (num1/10)%10 + num1%10 : num1;
         String result = num1 < 1000 ? "The result is " : "Enter a number less than zero";
         return result + sum;
 
--- src/Assignment02_Methods/A04_CheckEmail.java
+++ src/Assignment02_Methods/A04_CheckEmail.java
@@ -9,17 +9,20 @@ public class A04_CheckEmail {
         String email = scanner.nextLine();
 
         System.out.println(checkEmail(email));
+
     }
 
     public static String checkEmail(String email) {
-        String result = !email.contains("@") ? email+ " is not a valid email address" :
-                        !email.contains("@gmail.com") ?  " Enter your email address : " :
+        String result = !email.contains("@") ? email + " is not a valid email address" :
+                !email.contains("@gmail.com") ? " Enter your email address : " :
                         !email.endsWith("@gmail.com") ? " Wrong spelling check your email address" :
-                        " It appears to be a valid email address.";
+                                " It appears to be a valid email address.";
         return result;
 
 
     }
+}
+
     /*public static String checkEmail2(String email) {
         String result = !email.contains("@") ? email+ " is not a valid email address" :
                 !email.contains("@gmail.com") ?  checkEmail2(email) :
@@ -29,4 +32,4 @@ public class A04_CheckEmail {
 
 
     }*/
-}
+
