b3a01ed8339b485c1b5be16fa1d9b4b0e24836c5
CodeDebugRun <c7105erhan@gmail.com>
2022-07-04 19:15:31 +0300

    regex added

--- src/Assignment02_Methods/A04_CheckEmail.java
+++ src/Assignment02_Methods/A04_CheckEmail.java
@@ -1,6 +1,7 @@
 package Assignment02_Methods;
 
 import java.util.Scanner;
+import java.util.regex.*;
 
 public class A04_CheckEmail {
     public static void main(String[] args) {
@@ -9,6 +10,30 @@ public class A04_CheckEmail {
         String email = scanner.nextLine();
 
         System.out.println(checkEmail(email));
+        // Test Case 1:
+        String str1 = "Geeks@portal20";
+        System.out.println(isValidPassword(str1));
+
+        // Test Case 2:
+        String str2 = "Techpro Education";
+        System.out.println(isValidPassword(str2));
+
+        // Test Case 3:
+        String str3 = "tech9";
+        System.out.println(isValidPassword(str3));
+
+        // Test Case 4:
+        String str4 = "1234";
+        System.out.println(isValidPassword(str4));
+
+        // Test Case 5:
+        String str5 = "tc@20";
+        System.out.println(isValidPassword(str5));
+
+        // Test Case 6:
+        String str6 = "techs@portal20";
+        System.out.println(isValidPassword(str6));
+
 
     }
 
@@ -21,15 +46,40 @@ public class A04_CheckEmail {
 
 
     }
-}
+    public static boolean isValidPassword(String password)
+    {
 
-    /*public static String checkEmail2(String email) {
-        String result = !email.contains("@") ? email+ " is not a valid email address" :
-                !email.contains("@gmail.com") ?  checkEmail2(email) :
-                        !email.endsWith("@gmail.com") ? " Wrong spelling check your email address" :
-                                " It appears to be a valid email address.";
-        return result;
+        // Regex to check valid password.
+        String regex = "^(?=.*[0-9])"
+                + "(?=.*[a-z])(?=.*[A-Z])"
+                + "(?=.*[@#$%^&+=])"
+                + "(?=\\S+$).{8,20}$";
+
+        // Compile the ReGex
+        Pattern p = Pattern.compile(regex);
+
+        // If the password is empty
+        // return false
+        if (password == null) {
+            return false;
+        }
+
+        // Pattern class contains matcher() method
+        // to find matching between given password
+        // and regular expression.
+        Matcher m = p.matcher(password);
+
+        // Return if the password
+        // matched the ReGex
+        return m.matches();
+    }
+    // Driver Code.
+    //public static void main(String args[])
 
 
-    }*/
+
+    }
+
+
+
 
---
+++ src/Assignment02_Methods/A06Regex.java
@@ -0,0 +1,31 @@
+package Assignment02_Methods;
+
+import java.util.Scanner;
+
+public class A06Regex {
+    public static void main(String[] args) {
+
+      Scanner scanner = new Scanner (System.in);
+        //System.out.println("enter password");
+        //String password = scanner.nextLine();
+        String password2 = "ABCabd123!@#$%^";
+        String password3 = "ABCabd123!@#$%^";
+
+        password2 = password2.replaceAll("[A-Z]", "a");
+        System.out.println(password2);
+        password2 = password2.replaceAll("[^A-Z]", "a");
+        System.out.println(password2);
+        password3 = password3.replaceAll("[a-z]", "z");
+        System.out.println(password3);
+        password3 = password3.replaceAll("[!-z]", "(");
+        System.out.println(password3);
+
+
+
+
+
+
+            }
+        }
+
+
