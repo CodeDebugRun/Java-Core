fa68e6e9f8ecd56b5b34acb7b18714f2592a3ad0
CodeDebugRun <c7105erhan@gmail.com>
2022-07-05 14:49:49 +0300

    Arrays added

---
+++ src/Day16_WhileLoop01/C01_GuessNumber.java
@@ -0,0 +1,42 @@
+package Day16_WhileLoop01;
+
+import java.util.Scanner;
+
+public class C01_GuessNumber {
+    public static void main(String[] args) {
+        //Print  543 54 and 5 on the console
+        /* for (int i = 1; i <= 5; i++) {
+            for (int j = (5-i); j > 1; j--) {
+                System.out.print(i+j);
+            }
+            System.out.println();
+        }*/
+
+        int target = (int) (Math.random() * 1000);
+        System.out.println(target);
+        int attempt = 10;
+
+        do {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter a number between 1-1000 to play :  ");
+            int number = scanner.nextInt();
+            if (number < 0 || number > 1000) {
+                attempt--;
+                System.out.println("Please enter a number between 1-1000");
+            } else if (number < target) {
+                attempt--;
+                System.out.println("Target is greater than " + number + ". Attempt left is " + attempt);
+            } else if (number > target) {
+                attempt--;
+                System.out.println("Target is less than " + number + ". Attempt left is " + attempt);
+            } else{
+                System.out.println("Remained attempt " + attempt);
+                System.out.println("Congrats you win");
+                //Burada attempt esittir sifirda calisiyor break de calisiyor;
+                //attempt = 0;
+                break;
+            }
+
+        } while (attempt > 0);
+    }
+}
---
+++ src/Day17_Arrays01/C01_Arrays.java
@@ -0,0 +1,7 @@
+package Day17_Arrays01;
+
+public class C01_Arrays {
+    public static void main(String[] args) {
+
+    }
+}
