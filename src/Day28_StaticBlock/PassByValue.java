9532e3dde8a4a02c57b90b1f113e1eb938790598
CodeDebugRun <c7105erhan@gmail.com>
2022-07-22 17:20:24 +0300

    Some Changes

---
+++ src/Day28_StaticBlock/PassByValue.java
@@ -0,0 +1,7 @@
+package Day28_StaticBlock;
+
+public class PassByValue {
+    public static void main(String[] args) {
+
+    }
+}
--- src/Day28_StaticBlock/StaticBlock.java
+++ src/Day28_StaticBlock/StaticBlock.java
@@ -8,9 +8,13 @@ public class StaticBlock {
                                                 //main dan once calisir
         }
 
+
     public static void main(String[] args) {
         System.out.println("Main works");
         System.out.println(sayi);
 
     }
+    static {
+        System.out.println("static under main method works before main"); //static olusturmak icin sadece static keyword
+    }                                                  //Birden fazla static yukaridan asagi dogru calisir
 }
